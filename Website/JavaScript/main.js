1a125ece9a0d0579a2aaad2cd6466d38478ee9b8
Darki002 <darkizockt16@gmail.com>
2023-02-13 11:26:24 +0100

    Einiges
    Burger Menu fertig
    Editor anfang

--- Website/JavaScript/main.js
+++ Website/JavaScript/main.js
@@ -1,9 +1,19 @@
 const themeButton = document.getElementById("theme-button");
+const themeButtonSmall = document.getElementById("theme-button-menu-small");
+
+themeButtonSmall?.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+
+    var darkModeActive = themeButtonSmall.checked;
+    themeButton.checked = darkModeActive;
+    localStorage.setItem("darkMode", darkModeActive)
+});
 
 themeButton?.addEventListener("click", () => {
     document.body.classList.toggle("dark-theme");
 
     var darkModeActive = themeButton.checked;
+    themeButtonSmall.checked = darkModeActive;
     localStorage.setItem("darkMode", darkModeActive)
 });
 
